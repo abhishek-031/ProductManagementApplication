81b4a9066671883c3412eb76eef643293f7f1f8f
abhishek-031 <cabhishek712@gmail.com>
2020-08-13 16:25:16 +0530

    add product

--- frontend/src/components/product/AddProduct.js
+++ frontend/src/components/product/AddProduct.js
@@ -2,10 +2,65 @@ import React from 'react';
 
 class AddProduct extends React.Component{
 
+  async handleSubmit(e){
+    e.preventDefault();
+    const product = {};
+    const form = document.forms['newProduct'];
+    product.name = form.name.value;
+    const categories = [];
+    this.categories.forEach(category=>{
+      if(form[category].checked){
+        categories.push(category);
+      }
+    });
+    product.category = categories;
+    product.availability = form.availability.value==='available'?true:false;
+    product.price = parseFloat(form.price.value);
+    product.quantity = parseInt(form.quantity.value);
+    await fetch('/users/admin/productList/createProduct',{
+      method:'post',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(product)
+    });
+    this.props.history.push('/products');
+  }
+
+  categories = [
+    'Transport',
+    'Electronics',
+    'Furniture',
+    'Entertainment',
+    'Fitness',
+    'Sports'
+  ];
+
   render(){
     return (
-      <form>
-        
+      <form name='newProduct' onSubmit={e=>this.handleSubmit(e)}>
+        <label htmlFor='name'>Name</label>
+        <input required type='text' name='name' id='name'/>
+        <br />
+
+        <label>Categories</label>
+        <br />
+        { this.categories.map(category => (
+          <label key={category}><input type='checkbox' name={category} id={category} value={category}/>{category}</label>
+        ))}
+
+        <br/>
+
+        <label htmlFor='availability'>Availability</label> <br/>
+        <input type='radio' name='availability' value='available' defaultChecked /> Available <br/>
+        <input type='radio' name='availability' value='unavailable' /> Unavailable <br/>
+
+        <label>Price</label>
+        <input required type='number' step='any' min={0} name='price'/> <br/>
+
+        <label>Quantity</label>
+        <input required type='number' min={0} name='quantity'/> <br/>
+        <button type='submit'>Add Product</button>
       </form>
     )
   }
